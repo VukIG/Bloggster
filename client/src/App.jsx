0f1c5a01a106450b99ec2b7e7acf0ad86dd06b5f
VukIG <vuk.ignjatovic05@gmail.com>
2023-09-16 17:37:19 +0200

    Promene

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Header from './Header';
 import Main from './Main';
 function App() {
   return (
-    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="bg-gradient-to-b from-blue-300 via-pink-400 to-red-400">
       <div className='p-5'>
         <Header />
         <Main/>
--- client/src/Article.jsx
+++ client/src/Article.jsx
@@ -2,14 +2,16 @@ import Dubrovnik from '../public/dubrovnik.jpg'
 function Article() {
   return (
     <div className="card w-96 glass">
-    <figure><img src={Dubrovnik}/></figure>
+      <div className="card w-96 bg-base-100">
+    <figure><img src={Dubrovnik} /></figure>
     <div className="card-body">
-        <h2 className="card-title">Life hack</h2>
-        <p>How to park your car at your garage?</p>
-        <div className="card-actions justify-end">
-        <button className="btn btn-primary">Learn now!</button>
-        </div>
+      <h2 className="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary">Buy Now</button>
+      </div>
     </div>
+</div>
     </div>
   )
 }
