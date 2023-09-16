893c674116df0a975538bf1ece43c30e70499cfc
VukIG <vuk.ignjatovic05@gmail.com>
2023-09-16 17:41:03 +0200

    Promenjeno ime iz Main u Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from './Header';
-import Main from './Main';
+import Section from './Section';
 function App() {
   return (
     <div className="bg-gradient-to-b from-blue-300 via-pink-400 to-red-400">
       <div className='p-5'>
         <Header />
-        <Main/>
+        <Section/>
       </div>  
     </div>
     
