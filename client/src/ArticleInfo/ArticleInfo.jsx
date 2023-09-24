e9b5cb619fd08cbaec81a602c29421ad988a3f1d
VukIG <vuk.ignjatovic05@gmail.com>
2023-09-24 14:14:28 +0200

    Zapocet Card details

--- client/src/ArticleInfo/ArticleInfo.jsx
+++ client/src/ArticleInfo/ArticleInfo.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
-
+import RandomItems from "./RandomItems";
+import MainArticleInfo from "./MainArticleInfo";
 function ArticleInfo() {
-  return <div>ArticleInfo</div>;
+  return (
+    <div className="flex flex-col shadow-xl rounded-full w-full h-full justify-center align-middle items-center">
+      <MainArticleInfo />
+      <RandomItems />
+    </div>
+  )
 }
 
 export default ArticleInfo;
---
+++ client/src/ArticleInfo/RandomItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RandomItem() {
+  return (
+    <div>RandomItem</div>
+  )
+}
+
+export default RandomItem
